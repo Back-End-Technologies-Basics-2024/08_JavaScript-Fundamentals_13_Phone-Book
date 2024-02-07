f90016d12a17f74d38700dbaa7e3cee58648e8f8
DesiZaharieva <135128496+DesiZaharieva@users.noreply.github.com>
2024-02-07 12:32:33 +0200

    Add files via upload

---
+++ 13_Phone Book.js
@@ -0,0 +1,20 @@
+function solve(input) {
+    let uniqueNames = {};
+
+    input.forEach(element => {
+
+        let keyValuePair = element.split(" ");
+        let name = keyValuePair[0];
+        let phone = keyValuePair[1];
+
+        uniqueNames[name] = phone;
+        
+    });
+
+    for(let key in uniqueNames) {
+        console.log(`${key} -> ${uniqueNames[key]}`);
+    }
+}
+
+
+solve(['Tim 0834212554','Peter 0877547887','Bill 0896543112','Tim 0876566344'])
